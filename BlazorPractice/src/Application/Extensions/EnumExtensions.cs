21f1bc72b48d61930e7e124f009d7549b16a4e2a
toyoda-divakk <79815702+toyoda-divakk@users.noreply.github.com>
2022-01-05 18:10:23 +0900

    アップロードサービスの仕組み

--- BlazorPractice/src/Application/Extensions/EnumExtensions.cs
+++ BlazorPractice/src/Application/Extensions/EnumExtensions.cs
@@ -3,8 +3,16 @@ using System.ComponentModel;
 
 namespace BlazorPractice.Application.Extensions
 {
+    /// <summary>
+    /// Enumに機能を追加する
+    /// </summary>
     public static class EnumExtensions
     {
+        /// <summary>
+        /// Enumの各要素に[Description("せつめい")]みたいなのが付いている場合、それを取得する
+        /// </summary>
+        /// <param name="val"></param>
+        /// <returns></returns>
         public static string ToDescriptionString(this Enum val)
         {
             var attributes = (DescriptionAttribute[])val.GetType().GetField(val.ToString()).GetCustomAttributes(typeof(DescriptionAttribute), false);
--- BlazorPractice/src/Infrastructure/Services/UploadService.cs
+++ BlazorPractice/src/Infrastructure/Services/UploadService.cs
@@ -7,26 +7,44 @@ namespace BlazorPractice.Infrastructure.Services
 {
     public class UploadService : IUploadService
     {
+        /// <summary>
+        /// リクエストのファイルを保存し、そのDBに格納するパスを返す
+        /// </summary>
+        /// <param name="request"></param>
+        /// <returns>DBに記録する相対パス</returns>
         public string UploadAsync(UploadRequest request)
         {
             if (request.Data == null) return string.Empty;
+
+            // リクエストからデータを取り出す
             var streamData = new MemoryStream(request.Data);
             if (streamData.Length > 0)
             {
+                // アップロード先のフォルダパス
+                // UploadTypeのEnumにはDescriptionにフォルダパスを格納しているので、取得する
                 var folder = request.UploadType.ToDescriptionString();
                 var folderName = Path.Combine("Files", folder);
-                var pathToSave = Path.Combine(Directory.GetCurrentDirectory(), folderName);
-                bool exists = System.IO.Directory.Exists(pathToSave);
+                var pathToSave = Path.Combine(Directory.GetCurrentDirectory(), folderName); // フルパスにする
+                
+                // ディレクトリが無ければ作成
+                bool exists = Directory.Exists(pathToSave);
                 if (!exists)
-                    System.IO.Directory.CreateDirectory(pathToSave);
+                    Directory.CreateDirectory(pathToSave);
+
+                // 保存先ファイルフルパス
                 var fileName = request.FileName.Trim('"');
                 var fullPath = Path.Combine(pathToSave, fileName);
+
+                // DBに記録するパス
                 var dbPath = Path.Combine(folderName, fileName);
                 if (File.Exists(dbPath))
                 {
+                    // 有効なファイル名を取得する（既に同じ名前のファイルがあれば、"(1)"のような名前を付加する）
                     dbPath = NextAvailableFilename(dbPath);
                     fullPath = NextAvailableFilename(fullPath);
                 }
+
+                // 保存
                 using (var stream = new FileStream(fullPath, FileMode.Create))
                 {
                     streamData.CopyTo(stream);
@@ -39,39 +57,54 @@ namespace BlazorPractice.Infrastructure.Services
             }
         }
 
+        /// <summary>
+        /// ファイル名が重複したときに付加する文字列パターン
+        /// </summary>
         private static string numberPattern = " ({0})";
 
+        /// <summary>
+        /// 次の有効なファイル名を取得する
+        /// 同じ名前のファイルがあれば、"ファイル名 (1)"のような名前を付加する
+        /// </summary>
+        /// <param name="path">ファイルパス</param>
+        /// <returns></returns>
         public static string NextAvailableFilename(string path)
         {
             // Short-cut if already available
             if (!File.Exists(path))
                 return path;
 
-            // If path has extension then insert the number pattern just before the extension and return next filename
+            // パスが拡張子を持つ場合、拡張子の直前に(1)のような番号パターンを挿入し、次のファイル名を返します。
             if (Path.HasExtension(path))
                 return GetNextFilename(path.Insert(path.LastIndexOf(Path.GetExtension(path)), numberPattern));
 
-            // Otherwise just append the pattern to the path and return next filename
+            // そうでなければ，単にパターンをパスに追加して，次のファイル名を返す。
             return GetNextFilename(path + numberPattern);
         }
 
+        /// <summary>
+        /// "ファイル名"を"ファイル名 (1)"のようにする
+        /// 空いている番号は二分探索で探す
+        /// </summary>
+        /// <param name="pattern">"ファイル名 ({0})"のようなパターン</param>
+        /// <returns></returns>
         private static string GetNextFilename(string pattern)
         {
             string tmp = string.Format(pattern, 1);
-            //if (tmp == pattern)
-            //throw new ArgumentException("The pattern must include an index place-holder", "pattern");
 
             if (!File.Exists(tmp))
-                return tmp; // short-circuit if no matches
+                return tmp;
 
-            int min = 1, max = 2; // min is inclusive, max is exclusive/untested
+            int min = 1, max = 2; // minは番号が埋まっていると分かっている所、maxは未検証部分
 
+            // ファイルがあれば2倍にして空いているファイル名を探す
             while (File.Exists(string.Format(pattern, max)))
             {
                 min = max;
                 max *= 2;
             }
 
+            // 2の乗数で空いている番号が見つかれば、二分探索で空いている番号を探す
             while (max != min + 1)
             {
                 int pivot = (max + min) / 2;
